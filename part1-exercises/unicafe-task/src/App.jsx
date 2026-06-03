d7f7b573d17ce80b629fe7fa7e4f6d018ed3f1ca
donya <donyaa@metropolia.fi>
2026-06-03 20:35:27 +0300

    task 1.7

--- part1-exercises/unicafe-task/src/App.jsx
+++ part1-exercises/unicafe-task/src/App.jsx
@@ -1,5 +1,51 @@
 import {useState} from 'react'
 
+// task 1.6
+// const Button = (props) => {
+//   return (
+
+//     <button onClick={props.onClick} > {props.text} </button>
+//   )
+
+
+// }
+
+// const App = () => {
+// const [goodCounter, setGoodCounter] = useState(0)
+// const [neutralCounter, setNeutralCounter] = useState(0)
+// const [badCounter, setBadCounter] = useState(0)
+
+// const handleGoodClick = () => {
+//   setGoodCounter(goodCounter + 1)
+// }
+
+// const handleNeutralClick = () => {
+//   setNeutralCounter(neutralCounter + 1)
+// }
+
+// const handleBadClick = () => {
+//   setBadCounter(badCounter + 1)
+// }
+
+// return (
+//   <div>
+//     <h1> give feedback </h1>
+
+//     <Button onClick={handleGoodClick} text='good' />
+//     <Button onClick={handleNeutralClick} text = 'neutral' />
+//     <Button onClick={handleBadClick} text = 'bad' />
+
+//     <h1> statistics </h1>
+//     <p> good {goodCounter}</p>
+//     <p> neutral {neutralCounter}</p>
+//     <p> bad {badCounter}</p>
+//   </div>
+// )
+
+// }
+
+// task 1.7
+
 const Button = (props) => {
   return (
 
@@ -26,6 +72,10 @@ const handleBadClick = () => {
   setBadCounter(badCounter + 1)
 }
 
+const all = goodCounter + neutralCounter + badCounter
+const average = (all / 3)
+const positivereviews = parseFloat((goodCounter / all) * 100).toFixed(2)
+
 return (
   <div>
     <h1> give feedback </h1>
@@ -38,6 +88,9 @@ return (
     <p> good {goodCounter}</p>
     <p> neutral {neutralCounter}</p>
     <p> bad {badCounter}</p>
+    <p> all {all} </p>
+    <p> average {average}</p>
+    <p> positive {positivereviews}%</p>
   </div>
 )
 
