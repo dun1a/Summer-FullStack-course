4dd9d53b73e2ed940dd8267652bd83a241de4d76
donya <donyaa@metropolia.fi>
2026-06-03 20:19:10 +0300

    task 1.6

--- fullstack-part1/src/App.jsx
+++ fullstack-part1/src/App.jsx
@@ -23,7 +23,7 @@
  */
 
 // using props (passing data to components)
-const Hello = (props) => {
+/* const Hello = (props) => {
   console.log(props)
   return (
 
@@ -60,6 +60,176 @@ const App = () => {
        
     </div>
   )
+} */
+
+
+//////// Component state, event handlers
+
+/* const Hello = (props) => {
+  // adding helper function for guessing age from current year
+  // function invoced when component is rendered 
+  // this helper function can access all the props of the component
+  const yearBorn = () => {
+    const yearNow = new Date().getFullYear()
+    return yearNow - props.age
+  }
+    return (
+        <div>
+            <p>
+                Hello {props.name}, you are {props.age} years old.
+            </p>
+            <p>
+              So you were probably born in {yearBorn()}.
+            </p>
+        </div>
+    )
 }
+
+const App = () => {
+    const name = 'Hoshi'
+    const age = 29
+
+    return (
+        <div>
+            <h1>Greetings</h1>
+            <Hello name = {name} age = {age} />
+            <Hello name = 'Jungwon' age = {22} />
+        </div>
+    )
+} */
+
+
+/* const Hello = (props) => {
+  // adding helper function for guessing age from current year
+  // function invoced when component is rendered 
+  // this helper function can access all the props of the component
+  const name = props.name
+  const age = props.age
+  const yearBorn = () => {
+    const yearNow = new Date().getFullYear()
+    return yearNow - age
+  }
+    return (
+        <div>
+            <p>
+                Hello {name}, you are {age} years old.
+            </p>
+            <p>
+              So you were probably born in {yearBorn()}.
+            </p>
+        </div>
+    )
+}
+
+const App = () => {
+  const props = {
+    name: 'Hoshi',
+    age: 29
+  } 
+    return (
+        <div>
+            <h1>Greetings</h1>
+            <Hello name = {props.name} age = {props.age} />
+            <Hello name = 'Jungwon' age = {22} />
+        </div>
+    )
+} */
+
+/////// pre rendering
+/*
+const App = (props) =>{
+  const {counter} = props // destructuring props object to get counter value
+  // or in <div> {props.counter} </div> without destructuring
+  return (
+    <div>
+      {counter}  // rendering the counter value, is used in 
+    </div>
+  )
+}*/
+
+////// state component, state hook
+
+/* import { useState } from 'react'
+
+const App = () => {
+  const [counter, setCounter] = useState(0) // state variable
+
+  const handleClick = () => {
+    console.log('Button clicked')
+    setCounter(counter + 1) // update state variable when button is clicked
+  }
+  //setTimeout(() => setCounter(counter + 1), 1000) // update state variable after 1 second
+
+  return (
+    <div>
+      {counter}
+      <button onClick = {handleClick}> event handler
+        Click me
+      </button>
+      <button onClick = {() => setCounter(0)}>
+        Reset
+      </button>
+      
+    </div>
+  )
+} */
+
+
+  //////  passing state t child components
+
+// import { useState } from 'react'
+//   // component to display counter value
+// const Display = (props) => {
+//   return (
+//     <div>
+//       {props.counter}
+//     </div>
+//   )
+// }
+
+// const Button = (props) => {
+
+//   return (
+//     <button onClick = {props.onClick} > {props.text} </button>
+//   )
+// }
+
+// const App = () => {
+
+//   const [counter, setCounter] = useState(0) // sets initial value when the App component is rendered
+
+//   const setToZero = () => setCounter(0)
+//   const decreaseByOne = () => setCounter(counter - 1)
+//   const increaseByOne = () => setCounter(counter + 1)
+
+//   return (
+//     <div>
+//       <Display counter = {counter} />
+//       {/* <button onClick= {increaseByOne} >increment</button>
+//       <button onClick = {setToZero}>reset</button> */}
+//       <Button onClick={increaseByOne} text="increment"/>
+//       <Button onClick = {setToZero} text = "reset"/>
+//       <Button onClick = {decreaseByOne} text = "decrease"/>
+//     </div>
+//   )
+// }
+
+///////// complex state
+import { useState } from 'react'
+
+const App = () => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+
+  return (
+    <div>
+      {left}
+      <button onClick= {() => setLeft(left+1)}>left</button>
+      <button onClick= {() => setRight(right+1)}>right</button>
+      {right}
+    </div>
+  )
+}
+
 
 export default App
--- fullstack-part1/src/main.jsx
+++ fullstack-part1/src/main.jsx
@@ -4,11 +4,28 @@ import ReactDOM from 'react-dom/client'
 
 import App from './App.jsx'
 
+/* let counter = 0
 // renderes the App.jsx component's contents here, in the div-element in the index.html file with the id value root
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+const root = ReactDOM.createRoot(document.getElementById('root'))
 
+const refresh = () => {
+    root.render(<App counter = {counter} />)
+}
+
+setInterval (() => {
+    if (counter < 10) {
+        counter += 1
+        refresh()
+    }
+}, 1000)
+ */
 // createRoot(document.getElementById('root')).render(
 //   <StrictMode>
 //     <App />
 //   </StrictMode>,
 // )
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <App />
+)
+
