0a24d39eec53b30239d115f132a287cabec5ac06
donya <donyaa@metropolia.fi>
2026-06-03 20:45:47 +0300

    task 1.8

--- part1-exercises/unicafe-task/src/App.jsx
+++ part1-exercises/unicafe-task/src/App.jsx
@@ -46,13 +46,70 @@ import {useState} from 'react'
 
 // task 1.7
 
+// const Button = (props) => {
+//   return (
+
+//     <button onClick={props.onClick} > {props.text} </button>
+//   )
+
+
+// }
+
+// const App = () => {
+// const [goodCounter, setGoodCounter] = useState(0)
+// const [neutralCounter, setNeutralCounter] = useState(0)
+// const [badCounter, setBadCounter] = useState(0)
+
+// const handleGoodClick = () => {
+//   setGoodCounter(goodCounter + 1)
+// }
+
+// const handleNeutralClick = () => {
+//   setNeutralCounter(neutralCounter + 1)
+// }
+
+// const handleBadClick = () => {
+//   setBadCounter(badCounter + 1)
+// }
+
+// const all = goodCounter + neutralCounter + badCounter
+// const average = (all / 3)
+// const positivereviews = parseFloat((goodCounter / all) * 100).toFixed(2)
+
+// return (
+//   <div>
+//     <h1> give feedback </h1>
+
+//     <Button onClick={handleGoodClick} text='good' />
+//     <Button onClick={handleNeutralClick} text = 'neutral' />
+//     <Button onClick={handleBadClick} text = 'bad' />
+
+//     <h1> statistics </h1>
+//     <p> good {goodCounter}</p>
+//     <p> neutral {neutralCounter}</p>
+//     <p> bad {badCounter}</p>
+//     <p> all {all} </p>
+//     <p> average {average}</p>
+//     <p> positive {positivereviews}%</p>
+//   </div>
+// )
+
+// }
+
+// task 1.8
 const Button = (props) => {
   return (
 
     <button onClick={props.onClick} > {props.text} </button>
   )
+}
 
-
+const Statistics = (props) => {
+  return (
+    <div>
+      <p> {props.text} {props.result}</p>
+    </div>
+  )
 }
 
 const App = () => {
@@ -74,7 +131,14 @@ const handleBadClick = () => {
 
 const all = goodCounter + neutralCounter + badCounter
 const average = (all / 3)
-const positivereviews = parseFloat((goodCounter / all) * 100).toFixed(2)
+const positivereviews = () => {
+  if (all === 0) {
+    return 0
+  } else {
+    return parseFloat((goodCounter / all) * 100).toFixed(2)
+  }
+}
+
 
 return (
   <div>
@@ -88,9 +152,10 @@ return (
     <p> good {goodCounter}</p>
     <p> neutral {neutralCounter}</p>
     <p> bad {badCounter}</p>
-    <p> all {all} </p>
-    <p> average {average}</p>
-    <p> positive {positivereviews}%</p>
+
+    <Statistics text = 'all' result =  {all} />
+    <Statistics text = 'average' result = {average} />
+    <Statistics text = 'positive' result = {positivereviews()} />
   </div>
 )
 
