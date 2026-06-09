e74b56d946d748fc5797fa11a8df8640400b738e
donya <donyaa@metropolia.fi>
2026-06-09 20:22:26 +0300

    task 2.2

--- part2-exercises/courseInfo-tasks/part2a/src/components/Course.jsx
+++ part2-exercises/courseInfo-tasks/part2a/src/components/Course.jsx
@@ -1,15 +1,17 @@
 const Course = ({course}) => {
+    const total = course.parts.reduce((a,b) => a + b.exercises, 0)
 
     return (
         <div>
             <h1>{course.name}</h1>
-            <p>
-                {course.parts.map(part => 
-                    <li key ={part.id}>{part.name} {part.exercises}</li>
-                )}
-           </p>
-
             
+                {course.parts.map(part => 
+                    <p key ={part.id}>{part.name} {part.exercises}</p>
+                )}
+           
+            <h4>
+                Number of exercises {total}
+            </h4>
         </div>
     )
 }
