7daff4e4a5112f1206eccfb991b94cd76fdd53c3
donya <donyaa@metropolia.fi>
2026-06-14 00:55:28 +0300

    2.7

--- part2-exercises/thePhotobook-tasks/thePhotobool/src/App.jsx
+++ part2-exercises/thePhotobook-tasks/thePhotobool/src/App.jsx
@@ -10,12 +10,17 @@ function App() {
 
   const addPerson = (event) => {
     event.preventDefault()
+    
+    if (persons.some(person => person.name === newName)){
+      alert(`${newName} is already added to phonebook`)
+      return
+    }
+    
     const personObject = {
       name: newName
     }
     setPersons(persons.concat(personObject))
     setNewName('')
-
   }
 
   return (
