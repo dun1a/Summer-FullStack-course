2c5d6deb49fb9b5de53289537c059bedd2a2a5d6
donya <donyaa@metropolia.fi>
2026-06-23 18:57:33 +0300

    adding errors and separating backend logic

---
+++ fullstack-part2/part2/src/services/notes.js
@@ -0,0 +1,32 @@
+import axios from 'axios'
+
+const baseUrl = 'http://localhost:3001/notes'
+
+const getAll = () => {
+    const request = axios.get(baseUrl)
+    const noneExisting = {
+        id: 100000,
+        content: 'This note is not saved to the server',
+        important: true,
+    }
+    return request.then(response => response.data.concat(noneExisting))
+    //return axios.get(baseUrl) 
+}
+
+const create = newObject => {
+    const request = axios.post(baseUrl, newObject)
+    return request.then(response => response.data)
+    //return axios.post(baseUrl, newObject)
+}
+
+const update = (id, newObject) => {
+    const request = axios.put(`${baseUrl}/${id}`, newObject)
+    return request.then(response => response.data)
+    //return axios.put(`${baseUrl}/${id}`, newObject)
+}
+
+export default {
+    getAll,
+    create,
+    update
+}
