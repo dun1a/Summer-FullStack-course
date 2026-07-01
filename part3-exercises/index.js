184d8c442653d75f9f61b99bd677e54fea2823a5
donya <donyaa@metropolia.fi>
2026-07-01 23:38:11 +0300

    task 3.1-3.2

---
+++ part3-exercises/index.js
@@ -0,0 +1,44 @@
+import express from 'express';
+
+console.log("Testing testing testing")
+const app = express();
+app.use(express.json())
+
+let phonebook = [
+    { 
+      "id": "1",
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": "2",
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": "3",
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": "4",
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+app.get('/api/phonebook', (request,response) => {
+    response.json(phonebook)
+})
+
+app.get('/info', (request, response) => {
+    const html = `
+    <p>Phoneboook has info for ${phonebook.length} people</p>
+    <p>${new Date()}</p>
+    `
+    response.send(html)
+})
+
+const PORT = 3001;
+app.listen(PORT)
+console.log(`Server running on port ${PORT}`)
