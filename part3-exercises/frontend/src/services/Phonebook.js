0ba15d15b75a7ac7617fc6e4543ba0a545a0ea68
donya <donyaa@metropolia.fi>
2026-07-03 15:34:56 +0300

    fixing delete operation

--- part3-exercises/frontend/src/services/Phonebook.js
+++ part3-exercises/frontend/src/services/Phonebook.js
@@ -20,8 +20,14 @@ const update = (id, newObject) => {
     return request.then(response => response.data)    
 }
 
+const deletePerson = (id) => {
+    const request = axios.delete(`${baseUrl}/${id}`)
+    return request.then(response => response.data)
+}
+
 export default {
     getAll,
     create,
-    update
+    update,
+    deletePerson
 }
