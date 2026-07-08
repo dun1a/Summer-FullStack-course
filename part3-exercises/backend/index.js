a460302c7d63d97862bd2c7c2fce46a1d817698b
donya <donyaa@metropolia.fi>
2026-07-08 21:43:38 +0300

    task 3.13

--- part3-exercises/backend/index.js
+++ part3-exercises/backend/index.js
@@ -1,37 +1,24 @@
+import dotenv from 'dotenv'
 import express from 'express';
 import cors from 'cors';
+import connectDB from './mongo.js';
+import mongoose from 'mongoose'
+import Person from './model/PersonsModel.js'
 
-console.log("Testing testing testing")
+dotenv.config();
 const app = express();
+connectDB();
 app.use(cors());
 app.use(express.json())
 app.use(express.static('dist'))
 
-let phonebook = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
+let phonebook = [ ]
 
 app.get('/api/persons', (request,response) => {
-    response.json(phonebook)
+    Person.find({}).then(persons => {
+        response.json(persons)
+    })
+    
 })
 
 app.get('/info', (request, response) => {
@@ -43,21 +30,27 @@ app.get('/info', (request, response) => {
 })
 
 app.get('/api/persons/:id', (request, response) => {
-    const id = request.params.id
-    const person = phonebook.find(p => p.id === id)
-    
-    if(person){
-        response.json(person)
-    }else {
-        response.status(404).end()
-    }
+    Person.findById(request.params.id)
+    .then(person => {
+        if(person){
+            response.json(person)
+        }else{
+            response.status(404).end()
+        }
+    })
+    //const id = request.params.id
+    //const person = phonebook.find(p => p.id === id)
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = request.params.id
-    phonebook = phonebook.filter(p => p.id !== id)
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+        response.status(204).end()
+    })
+    .catch(error => next(error))
 
-    response.status(204).end()
+    //const id = request.params.id
+    //phonebook = phonebook.filter(p => p.id !== id)
 })
 
 app.post('/api/persons', (request, response) => {
@@ -75,15 +68,40 @@ app.post('/api/persons', (request, response) => {
         })
     }
 
-    const newPerson = {
-        id: Math.floor(Math.random() * 10000).toString(),
+    // save person according to the Person schema created
+    const newPerson = new Person({
+        //id: Math.floor(Math.random() * 10000).toString(),
         name: body.name,
         number: body.number
-    }
+    })
 
-    phonebook = phonebook.concat(newPerson)
-    console.log(phonebook)
-    response.json(newPerson)
+    newPerson.save().then(savedPerson => {
+        console.log('saved person', savedPerson)
+        response.json(savedPerson)
+    })
+    // phonebook = phonebook.concat(newPerson)
+    // console.log(phonebook)
+    // response.json(newPerson)
+})
+
+app.put('/api/persons/:id', (request,response, next) => {
+    const { name, number } = request.body
+
+    Person.findByIdAndUpdate(request.params.id)
+    .then(person => {
+        if(!person){
+            return response.status(404).end()
+        }
+
+        person.name = name
+        person.number = number
+        
+        return person.save()
+        .then(updatedPerson => {
+            response.json(updatedPerson)
+        })
+    })
+    .catch(error => next(error))
 })
 
 const PORT = process.env.PORT || 3001
---
+++ part3-exercises/backend/model/PersonsModel.js
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose'
+
+const personsSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    number: {
+        type: String,
+        required: true
+    }
+})
+
+personsSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    },
+
+})
+
+const Person = mongoose.model('Person', personsSchema)
+
+export default Person
