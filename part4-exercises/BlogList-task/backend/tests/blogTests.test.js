a5303b10cde84b714e074046223052963f74e7b0
donya <donyaa@metropolia.fi>
2026-07-14 01:13:32 +0300

    task 4.5

--- part4-exercises/BlogList-task/backend/tests/blogTests.test.js
+++ part4-exercises/BlogList-task/backend/tests/blogTests.test.js
@@ -1,6 +1,6 @@
 import {test, describe} from 'node:test'
 import assert from 'node:assert'
-import {dummy, totalLikes} from '../utils/list_helper.js'
+import {dummy, totalLikes, favoriteBlog} from '../utils/list_helper.js'
 
 describe('dummy', () => {
     test('dummy returns one', () => {
@@ -37,5 +37,31 @@ describe('totalLikes', () => {
 
         const result = totalLikes(blogs)
         assert.strictEqual(result, 0)
+    })
+})
+
+describe('favorite blog', () => {
+    test('favourite blog returns the one with most likes', () => {
+        const blogs = [
+            {title: 'Blog 1', likes: 5},
+            {title: 'Blog 2', likes: 10},
+            {title: 'Blog 3', likes: 2},
+            {title: 'Blog 4', likes: 15}
+        ]
+
+        const result = favoriteBlog(blogs)
+        assert.deepStrictEqual(result, {title: 'Blog 4', likes: 15})
+    })
+
+    test('favourite blog returns either one of the blogs with most likes', () => {
+        const blogs = [
+            {title: 'Blog 1', likes: 5},
+            {title: 'Blog 2', likes: 10},
+            {title: 'Blog 3', likes: 2},
+            {title: 'Blog 4', likes: 10}
+        ]
+
+        const result = favoriteBlog(blogs)
+        assert.deepStrictEqual(result.likes, 10)
     })
 })
