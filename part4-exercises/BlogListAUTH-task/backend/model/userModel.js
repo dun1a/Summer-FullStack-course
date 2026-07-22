9e5d4ad93de894525eb2ea46509c13a2193305cd
donya <donyaa@metropolia.fi>
2026-07-22 21:31:31 +0300

    task 4.15, creating new users

---
+++ part4-exercises/BlogListAUTH-task/backend/model/userModel.js
@@ -0,0 +1,29 @@
+import mongoose from 'mongoose'
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        minlength: 3
+    },
+    name: String,
+    passwordHash: {
+        type: String,
+        required: true,
+        minlength: 3
+    }
+})
+
+userSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+        delete returnedObject.passwordHash
+    }
+})
+
+const User = mongoose.model('User', userSchema)
+
+export default User
