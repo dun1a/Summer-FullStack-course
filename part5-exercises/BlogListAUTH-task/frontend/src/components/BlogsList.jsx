6dca00a909d2428578586f1a04c05e0fef355a4b
donya <donyaa@metropolia.fi>
2026-08-02 18:43:31 +0300

    task 5.11, (5.10 already done), sorting blogs by likes

--- part5-exercises/BlogListAUTH-task/frontend/src/components/BlogsList.jsx
+++ part5-exercises/BlogListAUTH-task/frontend/src/components/BlogsList.jsx
@@ -2,13 +2,14 @@ import Blog from './Blog.jsx'
 
 const BlogList = ({ blogs, updateLike }) => {
 
+    const sortedBlogs = blogs.sort((a, b) => b.likes - a.likes) 
 
     return (
         <div>
 
             <h2> Blogs </h2>
 
-            {blogs.map(blog => (
+            {sortedBlogs.map(blog => (
                 <Blog 
                     key = {blog.id} 
                     blog = {blog} 
